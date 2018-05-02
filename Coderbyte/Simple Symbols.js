aa4d82d99fae2093f0b7ed6e40a6a28ffa10c9f9
Hasan Khan <khanhasan12345@gmail.com>
2018-05-02 12:29:22 -0400

    Completed CoderByte: Simple Symbols problem

--- Coderbyte/Simple Symbols.js
+++ Coderbyte/Simple Symbols.js
@@ -4,15 +4,28 @@
 // Using the JavaScript language, have the function SimpleSymbols(str) take the str parameter being passed and determine if it is an acceptable sequence by either returning the string true or false. The str parameter will be composed of + and = symbols with several letters between them (ie. ++d+===+c++==a) and for the string to be true each letter must be surrounded by a + symbol. So the string to the left would be false. The string will not be empty and will have at least one letter. 
 // returns true if all letters in String parameter str are immediately preceeded by and immediately followed by +
 function SimpleSymbols(str) {
-	// boolean to be returned by function
-	bool = false;
+	// array containing booleans representing whether each individual letter in str is surrounded by +
+	var bools = [];
 	// loop over every character in str
-	for (var i = 0; i < str.length; ++) {
-		// if character in str is a letter and it's surrounded by +, set bool to true
-		if (((str[i].charCodeAt(0) >= 97 && str[i].charCodeAt(0) =< 122) || (str[i].charCodeAt(0) >= 65 && str[i].charCodeAt(0) =< 90)) && str[i - 1] === '+' && str [i + 1] === '+') {
+	for (var i = 0; i < str.length; i++) {
+		// if character in str is a letter
+		if (str[i].toLowerCase().charCodeAt(0) >= 97 && str[i].toLowerCase().charCodeAt(0) <= 122) {
+			// if character in str is a letter and it's surrounded by '+'s, push true to bools
+			// else push false to bools
+			if (str[i - 1] === '+' && str [i + 1] === '+') {
+				bools.push(true);
+			} else {
+				bools.push(false);
+			}
 		}
 	}
+	// loop over bools and return "true" if it contains all true booleans and "false" otherwise
+	for (var j = 0; j < bools.length; j++) {
+		if (bools[j] === false) {
+			return "false";
+		}
+	}
+	return "true";
 }
 // keep this function call here 
-SimpleSymbols(readline());
-/////////////////////////
+SimpleSymbols("++d+===+c++==a");
