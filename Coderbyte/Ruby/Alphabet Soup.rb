1824f352aa3b231e0aaa41ec41239eff688f55b9
Hasan Khan <khanhasan12345@gmail.com>
2018-05-05 09:48:20 -0400

    completed Alphabet Soup problem in Ruby

--- Coderbyte/Ruby/Alphabet Soup.rb
+++ Coderbyte/Ruby/Alphabet Soup.rb
@@ -3,9 +3,10 @@
 # https://coderbyte.com/editor/Alphabet%20Soup:Ruby
 # Using the Ruby language, have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string.
 
-# takes string parameter str and rearranges all letters in alphabetical order and returns it
+# takes string parameter str, rearranges all letters in alphabetical order, and returns new rearranged str
 def AlphabetSoup(str)
-	
+	# splits str into an array of the characters making up str, sorts that arr, and then converts it to a single string
+	str.chars.sort.join('')
 end
 # keep this function call here    
-puts AlphabetSoup(STDIN.gets)
+puts AlphabetSoup("hello")
