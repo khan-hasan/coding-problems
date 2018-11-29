4d08fd54157638ad4e195518c9ae050c2a6afdab
Hasan Khan <hkhan@simplepart.com>
2018-11-29 09:09:12 -0500

    Create opposite_number.cs

---
+++ C#/opposite_number.cs
@@ -0,0 +1,22 @@
+/*
+https://www.codewars.com/kata/opposite-number/train/csharp
+
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+But can you do it in 1 line of code and without any conditionals?
+*/
+
+using System;
+
+public class Kata
+{
+    public static int Opposite(int number)
+    {
+        return -1 * number;
+    }
+}
