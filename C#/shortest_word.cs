a67c14e2095708150e3f879f91ea008a94830933
Hasan Khan <hkhan@simplepart.com>
2018-11-30 12:56:11 -0500

    return length of shortest word from string of words

---
+++ C#/shortest_word.cs
@@ -0,0 +1,48 @@
+/*
+https://www.codewars.com/kata/shortest-word/train/csharp
+
+Difficulty: 7 kyu
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+public class Kata
+{
+  public static int FindShort(string s)
+  {
+    string[] words = string.Split(" ");
+    string shortest = words[0];
+
+    foreach (string word in words)
+    {
+        if (word.Length < words[0].Length)
+        {
+            shortest = word;
+        } 
+    }
+    return shortest.Length;
+  }
+}
+
+/**************************************************/
+/**************************************************/
+/**************************************************/
+
+namespace Solution 
+{
+  using NUnit.Framework;
+  using System;  
+
+  [TestFixture]
+  public class KataTests
+  {
+    [Test]
+    public void BasicTests()
+    {
+      Assert.AreEqual(3, Kata.FindShort("bitcoin take over the world maybe who knows perhaps"));
+      Assert.AreEqual(3, Kata.FindShort("turns out random test cases are easier than writing out basic ones"));      
+    }
+  }
+}
