0c9f40280e92ca2cb71c1ab8e41e7788e1c272dd
Hasan Khan <hkhan@simplepart.com>
2018-12-18 16:35:18 -0500

    Debugging roman_numerals_decoder.cs to run correctly.

--- C#/roman_numerals_decoder.cs
+++ C#/roman_numerals_decoder.cs
@@ -24,15 +24,61 @@ Courtesy of rosettacode.org
 */
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 public class RomanDecode
 {
     public static int Solution(string roman)
     {
-        // Hashtable representing the numeric value of each roman numeral
-        Hashtable 
+        // Hashtable representing the numeric value of each roman numeral key
+        Hashtable romanKeys = new Hashtable() {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
 
+        // If roman is one numeral, return numeric value
+        if (roman.Length == 1)
+        {
+            return (int)romanKeys[roman.ToCharArray()[0]];
+
+        }
+        else
+        {
+            // Array of chars in roman in reverse order
+            char[] romanChars = roman.ToCharArray();
+            Array.Reverse(romanChars);
+
+            // Numeric value of roman
+            int total = 0;
+
+            // Loop over each numeral in romanChars
+            for (int i = 0; i < romanChars.Length; i++)
+            {
+                Console.WriteLine("\nWe are now inside the for loop, and i = " + i.ToString() + ".");
+
+                // If the numeral at the following index is less than the current numeral, subtract them, and add result to total
+                if ((int)romanKeys[romanChars[i + 1]] < (int)romanKeys[romanChars[i]])
+                {
+                    Console.WriteLine("We are inside the if statement.");
+                    total =+ ((int)romanKeys[romanChars[i]] - (int)romanKeys[romanChars[i + 1]]);
+                    Console.WriteLine("total = " + total.ToString());
+                    i += 1;
+                }
+                else
+                {
+                    Console.WriteLine("We are inside the else statement.");
+                    total += ((int)romanKeys[romanChars[i]]);
+                    Console.WriteLine("total = " + total.ToString());
+                }
+            }
+            return total;
+        }
         throw new NotImplementedException();
     }
 }
