93ea9ebb2bdb3e722cbdf72d8502d2759ce705bf
Hasan Khan <khanhasan12345@gmail.com>
2018-12-26 10:29:09 -0500

    Started delete_a_digit.cs.

---
+++ C#/delete_a_digit.cs
@@ -0,0 +1,61 @@
+/*
+https://www.codewars.com/kata/simple-fun-number-79-delete-a-digit/train/csharp
+
+Difficulty: 6 kyu
+
+Task
+Given an integer n, find the maximal number you can obtain by deleting exactly one digit of the given number.
+
+Example
+For n = 152, the output should be 52;
+
+For n = 1001, the output should be 101.
+
+Input/Output
+[input] integer n
+
+Constraints: 10 ≤ n ≤ 1000000.
+
+[output] an integer
+PUZZLESGAMES
+*/
+
+namespace myjinxin
+{
+    using System;
+
+    public class Kata
+    {
+        public int DeleteDigit(int n)
+        {
+        }
+    }
+}
+
+/**************************************************/
+/**************************************************/
+/**************************************************/
+
+/*
+namespace myjinxin
+{
+    using NUnit.Framework;
+    using System;
+    
+    [TestFixture]
+    public class myjinxin
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata=new Kata();
+
+            Assert.AreEqual(52,kata.DeleteDigit(152));
+            
+            Assert.AreEqual(101,kata.DeleteDigit(1001));
+            
+            Assert.AreEqual(1,kata.DeleteDigit(10));
+        }
+    }
+}
+*/
