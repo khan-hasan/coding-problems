cb5969ba99cf059cfa69def695fc19c2471bd40d
Hasan Khan <khanhasan12345@gmail.com>
2019-01-14 21:54:49 -0500

    Completed return_negative.py.

---
+++ Python/return_negative.py
@@ -0,0 +1,32 @@
+'''
+https://www.codewars.com/kata/return-negative
+
+Difficulty: 8 kyu
+
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Example:
+
+make_negative(1);  # return -1
+make_negative(-5); # return -5
+make_negative(0);  # return 0
+Notes:
+
+The number can be negative already, in which case no change is required.
+Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+FUNDAMENTALS, NUMBERS
+'''
+
+def make_negative( number ):
+    if number < 0:
+        return number
+    else:
+        return -1*number
+
+###################################################
+###################################################
+###################################################
+
+'''
+Test.assert_equals(make_negative(42),-42)
+'''
