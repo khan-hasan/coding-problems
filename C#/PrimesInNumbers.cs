cfeae18f4b8e1c96aee331d8610e38c7e4dba1cd
Hasan Khan <hkhan@simplepart.com>
2019-02-01 16:40:43 -0500

    Started PrimesInNumbers.cs.

---
+++ C#/PrimesInNumbers.cs
@@ -0,0 +1,43 @@
+/*
+https://www.codewars.com/kata/primes-in-numbers/train/csharp
+
+Difficulty: 5 kyu
+
+Given a positive number n > 1 find the prime factor decomposition of n. The result will be a string with the following form :
+
+ "(p1**n1)(p2**n2)...(pk**nk)"
+with the p(i) in increasing order and n(i) empty if n(i) is 1.
+
+Example: n = 86240 should return "(2**5)(5)(7**2)(11)"
+
+FUNDAMENTALS, NUMBERS, MATHEMATICS, ALGORITHMS, UTILITIES
+*/
+
+public class PrimeDecomp
+{
+    public static String factors(int lst)
+    {
+
+    }
+}
+
+/**************************************************/
+/**************************************************/
+/**************************************************/
+
+/*
+using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class PrimeDecompTests
+{
+
+    [Test]
+    public void Test1()
+    {
+        int lst = 7775460;
+        Assert.AreEqual("(2**2)(3**3)(5)(7)(11**2)(17)", PrimeDecomp.factors(lst));
+    }
+}
+*/
