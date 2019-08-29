82d2320b3a5f5ea00b0ccabb512e119074ebfd5e
Hasan Khan <hasan.khan@macadmins-MacBook-Pro.local>
2019-08-28 21:49:39 -0400

    Solved snail_sort.py.

--- snail_sort.py
+++ snail_sort.py
@@ -4,7 +4,8 @@ https://www.codewars.com/kata/snail/train/python
 Difficulty: 4 kyu
 
 Snail Sort
-Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+Given an n x n array, return the array elements arranged from outermost elements to
+the middle element, traveling clockwise.
 
 array = [[1,2,3],
          [4,5,6],
@@ -18,7 +19,8 @@ array = [[1,2,3],
 snail(array) #=> [1,2,3,4,5,6,7,8,9]
 This image will illustrate things more clearly:
 
-NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+NOTE: The idea is not sort the elements from the lowest value to the highest; the idea
+is to traverse the 2-d array in a clockwise snailshell pattern.
 
 NOTE 2: The 0x0 (empty matrix) is represented as [[]]
 
@@ -27,81 +29,65 @@ ALGORITHMS, ARRAYS
 
 def snail(array):
     """
-    Sort an n x n array with elements arranged from outermost elements to the middle element, traveling clockwise.
+    Sort an n x n array with elements arranged from outermost elements to the middle element,
+    traveling clockwise.
 
     :param array: a list of lists representing the n x n array
     :type array: list of lists
 
-    :returns: a list with the elements of `array` arranged from outermost elements to the middle element, traveling clockwise
+    :returns: a list with the elements of `array` arranged from outermost elements to the
+    middle element, traveling clockwise
     :rtype: list
     """
 
-    n = len(array)
+    array_len = len(array)
     sorted_array = []
 
-    # Row index
+    #  Start traversing from the top-right corner of `array`.
     i = 0
-    # Column index. Start traversing from the top-right corner of `array`.
-    j = n - 1
+    j = array_len - 1
 
-    consec_same_dist = 0
-
-    dist_to_travel = n - 1
+    # Sequence of consecutive places to move after making a clockwise turn
+    dist_to_travel = sorted([dist for dist in range(1, array_len)] * 2, reverse=True)
 
     direction_to_travel = ['D', 'L', 'U', 'R']
     idx = 0
 
-    if len(array[0]) == 0:
+    if not array[0]:
         # If `array` has no elements, return an empty matrix.
-        return [[]]
+        return []
 
-    elif len(array[0]) == 1:
+    if len(array[0]) == 1:
         # If array has 1 element, return `array`.
         return [array[0][0]]
 
     # Traverse the first row in `array`.
-    for k in range(n):
+    for k in range(array_len):
         sorted_array.append(array[0][k])
-    
-    import pdb; pdb.set_trace()
-    while len(sorted_array) < n**2:
 
-        direction = direction_to_travel[idx]
+    for dist in dist_to_travel:
 
-        for _ in range(dist_to_travel):
+        # Reset index to beginning if at the end of `direction_to_travel`.
+        if idx == 4:
+            idx = 0
 
-            if direction == 'R':
-                j += 1
-            elif direction == 'D':
+        # Move `dist` units in the specified `direction_to_travel`.
+        for _ in range(dist):
+
+            direction = direction_to_travel[idx]
+            if direction == 'D':
                 i += 1
             elif direction == 'L':
                 j -= 1
-            else:
+            elif direction == 'U':
                 i -= 1
+            else:
+                j += 1
 
-            next_element = array[i][j]
-            sorted_array.append(next_element)
-        
-        # Cycle through the directions in clockwise order.
-        if idx == 3:
-            idx = 0
-        else:
-            idx += 1
+            current_element = array[i][j]
+            sorted_array.append(current_element)
 
-        if consec_same_dist == 2:
-            consec_same_dist = 0
-            dist_to_travel -= 1
-        else:
-            consec_same_dist += 1
-        
+        # Make clockwise turn around a corner.
+        idx += 1
+
     return sorted_array
-
-
-array = [[1,2,3,1],
-         [4,5,6,4],
-         [7,8,9,7],
-         [7,8,9,7]]
-
-# array = [[1]]
-print(snail(array))
-print(f'expected =\n[1, 2, 3, 1, 4, 7, 7, 9, 8, 7, 7, 4, 5, 6, 9, 8]')
