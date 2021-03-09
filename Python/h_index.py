df4457598ddb868723272a9feb02b06daf6885b9
Hasan Khan <hasan.khan@mhsglobal.com>
2021-03-08 22:21:32 -0500

    Solve h-index problem.

---
+++ Python/h_index.py
@@ -0,0 +1,22 @@
+"""
+The h-index is a metric that measures both the productivity and citation impact of a researcher. Specifically, a
+researcher's h-index is the largest number h such that the researcher has published h papers that have each been cited
+at least h times. For example, if Carl has published papers A,B,C,D,E,F,G,H,I, which have been cited 1,4,1,4,2,1,3,5,6
+times, respectively, then his h-index is 4 (corresponding to the papers B,D,H,I).
+
+Suppose you were asked to design an algorithm that determines a researcher's h-index.
+
+Elements of Programming Interviews in Python, Introduction, Page 1
+"""
+
+
+def find_h_index(arr):
+    arr = sorted(arr)
+    for i in range(1, len(arr)):
+        h_index = arr[-i]
+        if i == h_index:
+            return h_index
+
+
+if __name__ == '__main__':
+    assert find_h_index([1, 4, 1, 4, 2, 1, 3, 5, 6]) == 4
